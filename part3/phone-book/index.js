d6c88976becaaa450db98e2ac8640d112fde7f98
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-03-04 19:39:01 -0800

    finished part3 3.3 phonebook get a single person entry

--- part3/phone-book/index.js
+++ part3/phone-book/index.js
@@ -38,7 +38,20 @@ app.get('/info', (request, response) => {
     <p>Phonebook has info for ${numberOfPerson} people</p>
     <p>${currTime}</p>
     `)
-})
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => {
+        return person.id === id;
+    });
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+    response.json(note);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
