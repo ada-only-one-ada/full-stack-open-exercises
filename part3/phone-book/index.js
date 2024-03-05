d085f13913130e54013ebe416ab90853f4560a08
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-03-04 19:50:25 -0800

    finished part3 3.5 phonebook add a new entry by POST request

--- part3/phone-book/index.js
+++ part3/phone-book/index.js
@@ -57,7 +57,33 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id);
     persons = persons.filter(person => person.id !== id);
     response.status(204).end();
-})
+});
+
+const generateId = () => {
+    return Math.floor(Math.random() * 100000);
+}
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        });
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+    persons = persons.concat(person);
+    response.json(person);
+});
+
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
